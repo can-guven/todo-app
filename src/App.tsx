96d31e620507bdfa7d29cd951ca30d17d4623f95
can-guven <codecanguven@gmail.com>
2022-12-11 16:52:24 +0300

    added eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
-import './assets/styles/css/bootstrap-grid.min.css'
-import './assets/styles/sass/index.scss';
+import "./assets/styles/css/bootstrap-grid.min.css";
+import "./assets/styles/sass/index.scss";
 
 function App() {
-  return (
-    <div>
-      selam
-      </div>
-  );
+  return <div>selam</div>;
 }
 
 export default App;
